1d2d2a4c7f7cff04e78d061579404e597596933d
Kaivalya Kate <6522kaivalya@gmail.com>
2023-06-27 10:53:40 -0400

    new commit

---
+++ InterviewPreparation/group_anagram.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        a_map = dict()
+        for s in strs:
+            anagram = ''.join(i for i in sorted(s))
+            anagrams = a_map.get(anagram, None)
+            if anagrams:
+                anagrams.append(s)
+            else:
+                a_map[anagram] = [s]
+        return list(a_map.values())
+
+
+print(Solution().groupAnagrams(["a"]))
+print("Hello")
